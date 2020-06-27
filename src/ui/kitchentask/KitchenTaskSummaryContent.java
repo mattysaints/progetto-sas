ed7750899eea6f8ed77e0c112c08a2e2cb889c79
Davide Zago <zago.davide@educ.di.unito.it>
2020-06-27 19:20:55 +0200

    Avanzamento nella UI. Aggiunto kitchen task content

---
+++ src/ui/kitchentask/KitchenTaskSummaryContent.java
@@ -0,0 +1,29 @@
+package ui.kitchentask;
+
+import javafx.fxml.FXML;
+
+public class KitchenTaskSummaryContent {
+    @FXML
+    private void exitButtonPressed() {
+    }
+
+    @FXML
+    private void addKitchenTaskPressed() {
+    }
+
+    @FXML
+    private void deleteKitchenTaskPressed() {
+    }
+
+    @FXML
+    private void editKitchenTaskPressed() {
+    }
+
+    @FXML
+    private void ordinaDifficoltaPressed() {
+    }
+
+    @FXML
+    private void ordinaTempiPressed() {
+    }
+}
